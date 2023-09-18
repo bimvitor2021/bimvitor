8232d62f89ff562417e28fe417dd162d1818874d
bimvitor2021 <142360288+bimvitor2021@users.noreply.github.com>
2023-09-18 19:24:32 -0300

    Add files via upload

---
+++ Aula 4-Fabio/src/aula/pkg4/fabio/Aula4Fabio.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula.pkg4.fabio;
+
+/**
+ *
+ * @author 91944945687
+ */
+public class Aula4Fabio {
+   
+    public static void main(String[] args) {
+        int a = 10;
+        String b  = "10";
+        int c = 2;
+       
+    
+        int resultadoA = a + c;
+        int resultadoB = a - c;
+        int resultadoC = a * c;
+        int resultadoD = a / c;
+         int resultadoE = a % c;
+          int resultadoF = (int)a / c;
+    
+    System.out.println(resultadoA);
+        System.out.println(resultadoB);
+            System.out.println(resultadoC);
+                System.out.println(resultadoD);
+                 System.out.println(resultadoE);
+                  System.out.println(resultadoF);
+        
+        
+    }
+    
+}
